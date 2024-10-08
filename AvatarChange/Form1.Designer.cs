8f7d3ddcc2ec262e0f835961cb7a049172b45cb2
xdek <134400911+XdekTV@users.noreply.github.com>
2024-10-08 20:30:54 +0200

    Add files via upload

--- AvatarChange/Form1.Designer.cs
+++ AvatarChange/Form1.Designer.cs
@@ -1,16 +1,9 @@
 ﻿namespace AvatarChange
 {
-    partial class Form1
+    public partial class Form1
     {
-        /// <summary>
-        /// Wymagana zmienna projektanta.
-        /// </summary>
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
-        /// Wyczyść wszystkie używane zasoby.
-        /// </summary>
-        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -20,14 +13,9 @@
             base.Dispose(disposing);
         }
 
-        #region Kod generowany przez Projektanta formularzy systemu Windows
-
-        /// <summary>
-        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
-        /// jej zawartości w edytorze kodu.
-        /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.label1 = new System.Windows.Forms.Label();
             this.linkLabel1 = new System.Windows.Forms.LinkLabel();
             this.button1 = new System.Windows.Forms.Button();
@@ -37,6 +25,9 @@
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.groupBox1.SuspendLayout();
@@ -136,12 +127,46 @@
             this.groupBox1.TabIndex = 7;
             this.groupBox1.TabStop = false;
             // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 11F);
+            this.label3.Location = new System.Drawing.Point(349, 57);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(50, 20);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.label4.Location = new System.Drawing.Point(350, 81);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(38, 15);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "label4";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.label5.Location = new System.Drawing.Point(350, 100);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(38, 15);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "label5";
+            this.label5.Visible = false;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(823, 463);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.label2);
@@ -149,6 +174,7 @@
             this.Controls.Add(this.linkLabel1);
             this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "Form1";
@@ -161,8 +187,6 @@
 
         }
 
-        #endregion
-
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.LinkLabel linkLabel1;
         private System.Windows.Forms.Button button1;
@@ -172,5 +196,8 @@
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
         private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
     }
 }
--- AvatarChange/Form1.cs
+++ AvatarChange/Form1.cs
@@ -5,9 +5,12 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.IO; // Dodano dla obsługi plików
+using System.Security.Principal; // Dodano dla użycia WindowsIdentity
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Microsoft.Win32; // Dodano dla użycia Registry
+using System.DirectoryServices.AccountManagement; // Dodano dla użycia PrincipalContext
 
 namespace AvatarChange
 {
@@ -27,10 +30,88 @@ namespace AvatarChange
 
             // Ładowanie obrazów z domyślnego folderu
             LoadImages(); // Dodanie metody ładowania obrazów
+
+            // Załaduj aktualny awatar użytkownika
+            LoadCurrentAvatar();
+
+            // Rejestracja zdarzenia ładowania formularza
+            this.Load += new EventHandler(Form1_Load); // Dodanie zdarzenia ładowania formularza
+
+            // Rejestracja zdarzenia zamykania formularza
+            this.FormClosing += new FormClosingEventHandler(Form1_FormClosing); // Dodanie zdarzenia zamykania formularza
+        }
+
+        // Metoda ustawiająca nazwę użytkownika w label3 oraz typ konta w label4
+        private async void Form1_Load(object sender, EventArgs e)
+        {
+            // Ustaw nazwę użytkownika w label3
+            label3.Text = $"{Environment.UserName}";
+
+            // Przenieś sprawdzanie grupy użytkownika i hasła do osobnych metod asynchronicznych
+            await Task.Run(() => CheckUserGroup());
+            await Task.Run(() => CheckUserPassword());
+        }
+
+        private void CheckUserGroup()
+        {
+            WindowsIdentity identity = WindowsIdentity.GetCurrent();
+            WindowsPrincipal principal = new WindowsPrincipal(identity);
+
+            // Sprawdzenie, do jakiej grupy należy użytkownik
+            if (principal.IsInRole(WindowsBuiltInRole.Administrator))
+            {
+                label4.Invoke((MethodInvoker)delegate { label4.Text = "Administrator"; });
+            }
+            else if (principal.IsInRole(WindowsBuiltInRole.User))
+            {
+                label4.Invoke((MethodInvoker)delegate { label4.Text = "Standard User"; });
+            }
+            else
+            {
+                label4.Invoke((MethodInvoker)delegate { label4.Text = "Other Group"; });
+            }
+        }
+
+        // Metoda sprawdzająca, czy konto użytkownika ma hasło
+        private void CheckUserPassword()
+        {
+            using (PrincipalContext context = new PrincipalContext(ContextType.Machine))
+            {
+                // Sprawdzenie bieżącego użytkownika
+                UserPrincipal user = UserPrincipal.FindByIdentity(context, Environment.UserName);
+                if (user != null)
+                {
+                    // Sprawdzenie, czy konto ma hasło
+                    bool hasPassword = user.LastPasswordSet.HasValue;
+
+                    // Wywołanie na UI wątek
+                    label5.Invoke((MethodInvoker)delegate {
+                        if (hasPassword)
+                        {
+                            label5.Text = "Password protected"; // Ustawienie tekstu label5
+                            label5.Visible = true; // Ustawienie label5 na widoczny
+                        }
+                        else
+                        {
+                            label5.Visible = false; // Ukryj label5, jeśli nie ma hasła
+                        }
+                    });
+                }
+                else
+                {
+                    label5.Invoke((MethodInvoker)delegate { label5.Visible = false; }); // Ukryj label5, jeśli nie znaleziono użytkownika
+                }
+            }
         }
 
         // Metoda zamykająca aplikację po kliknięciu przycisku Cancel
         private void button1_Click(object sender, EventArgs e)
+        {
+            Application.Exit(); // Zamknięcie aplikacji
+        }
+
+        // Obsługa zamknięcia aplikacji po kliknięciu "X"
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
         {
             Application.Exit(); // Zamknięcie aplikacji
         }
@@ -42,8 +123,8 @@ namespace AvatarChange
             OpenFileDialog openFileDialog = new OpenFileDialog
             {
                 Filter = "Image Files|*.bmp;*.jpg;*.jpeg;*.png",
-                Title = "Wybierz obraz",
-                Multiselect = true // Umożliwienie wyboru wielu obrazów
+                Title = "Select Picture",
+                Multiselect = false // Umożliwienie wyboru jednego obrazu
             };
 
             // Wyświetl okno dialogowe i sprawdź, czy użytkownik wybrał plik
@@ -53,6 +134,66 @@ namespace AvatarChange
                 {
                     AddImageToPanel(file);
                 }
+            }
+        }
+
+        // Metoda ładująca aktualny awatar użytkownika z rejestru
+        private void LoadCurrentAvatar()
+        {
+            try
+            {
+                // Otwórz klucz rejestru
+                using (RegistryKey usersKey = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\AccountPicture\Users"))
+                {
+                    if (usersKey != null)
+                    {
+                        // Pobierz SID użytkownika
+                        string userSid = WindowsIdentity.GetCurrent().Owner.ToString();
+                        Console.WriteLine("User SID: " + userSid); // Debugowanie SID
+
+                        // Sprawdź, czy istnieje wpis dla aktualnego SID
+                        using (RegistryKey userKey = usersKey.OpenSubKey(userSid))
+                        {
+                            if (userKey != null)
+                            {
+                                string userPicturePath = userKey.GetValue("Image192")?.ToString();
+                                Console.WriteLine("User Picture Path: " + userPicturePath); // Debugowanie ścieżki do obrazu
+
+                                // Sprawdź, czy plik istnieje
+                                if (!string.IsNullOrEmpty(userPicturePath) && File.Exists(userPicturePath))
+                                {
+                                    pictureBox1.Image = Image.FromFile(userPicturePath);
+                                    // Ustaw rozmiar pictureBox1 na 56x56
+                                    pictureBox1.Width = 56;
+                                    pictureBox1.Height = 56;
+                                    pictureBox1.SizeMode = PictureBoxSizeMode.Zoom; // Upewnij się, że obrazek zachowuje proporcje
+                                }
+                                else
+                                {
+                                    // Ustaw pusty obrazek lub wyświetl komunikat, jeśli nie znaleziono pliku
+                                    pictureBox1.Image = null; // Ustawia obrazek na pusty
+                                    MessageBox.Show("Nie znaleziono awatara użytkownika. Ścieżka: " + userPicturePath);
+                                }
+                            }
+                            else
+                            {
+                                // Dodano komunikat debugujący, który wyświetla dostępne SID w rejestrze
+                                string[] subKeyNames = usersKey.GetSubKeyNames();
+                                MessageBox.Show("Nie znaleziono wpisu rejestru dla SID: " + userSid +
+                                                "\nDostępne SID w rejestrze: " + string.Join(", ", subKeyNames));
+                            }
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Nie znaleziono klucza rejestru dla obrazów konta użytkownika.");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Wystąpił błąd podczas ładowania awatara: " + ex.Message);
+                pictureBox1.Image = null; // Ustawia obrazek na pusty
             }
         }
 
@@ -119,6 +260,8 @@ namespace AvatarChange
             // Zaznacz nowy PictureBox
             selectedPictureBox = sender as PictureBox; // Przypisz wybrany PictureBox do zmiennej
             selectedPictureBox.Invalidate(); // Wymuś ponowne narysowanie
+
+            // Teraz można dodać dodatkowe operacje na zaznaczonym PictureBox, jeśli jest to wymagane
         }
 
         // Metoda rysująca zaznaczenie w PictureBox
@@ -131,7 +274,7 @@ namespace AvatarChange
             {
                 using (var pen = new Pen(Color.Blue, 2)) // Ustawienie koloru i grubości ramki
                 {
-                    e.Graphics.DrawRectangle(pen, 0, 0, pictureBox.Width - 1, pictureBox.Height - 1);
+                    e.Graphics.DrawRectangle(pen, 0, 0, pictureBox.Width - 2, pictureBox.Height - 2);
                 }
             }
         }
